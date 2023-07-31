41804230fc37ccb33601c1b4d4f023473b8503fe
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-07-31 08:20:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your JS code here. If required.
 const fileNames = [
   "file1.txt",
   "file2.txt",
@@ -7,5 +6,30 @@ const fileNames = [
   "file5.txt",
 ];
 
-// do not change the code above
-// add your code here
+async function fetchFileContent(fileName) {
+  try {
+    const response = await fetch(fileName);
+    if (!response.ok) {
+      throw new Error(`Error loading ${fileName}: ${response.status}`);
+    }
+    return await response.text();
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+async function readFilesAsync() {
+  const filePromises = fileNames.map(fetchFileContent);
+
+  try {
+    const fileContents = await Promise.all(filePromises);
+    fileContents.forEach((content, index) => {
+      console.log(`File ${index + 1}: ${content}`);
+    });
+  } catch (error) {
+    console.error("Error reading files:", error);
+  }
+}
+
+readFilesAsync();
